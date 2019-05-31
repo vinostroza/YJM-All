66a0434716cc32a1e8cf549e192a299a27a533d6
Wennona <50216937+WWWeLLs@users.noreply.github.com>
2019-05-31 10:20:45 -0500

    Add files via upload

---
+++ COLLABORATE/YJMscripts.js
@@ -0,0 +1,10 @@
+function footerDiv() {
+    let footer = document.getElementById("yjmFooter");
+    footer.style.backgroundColor = "white";
+    footer.style.color = "black";
+    footer.style.columnCount = "7";
+    footer.classList.add("footer");
+    footer.classList.add("footerAlign");
+ 
+ }
+ footerDiv();
