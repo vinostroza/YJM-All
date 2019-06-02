872d507dae9268ec6b6689e276272ef015513dfb
Wennona <50216937+WWWeLLs@users.noreply.github.com>
2019-06-02 02:07:13 -0500

    Code Update
    Code Updates for 6/1/19
    
    Co-Authored-By: vinostroza <vinostroza@users.noreply.github.com>
    Co-Authored-By: glor1 <glor1@users.noreply.github.com>
    Co-Authored-By: lwalker16 <lwalker16@users.noreply.github.com>

--- YJM Collaborate app/Scripts/YJMscripts.js
+++ YJM Collaborate app/Scripts/YJMscripts.js
@@ -1,11 +1,11 @@
 function footerDiv() {
     let footer = document.getElementById("yjmFooter");
-    footer.style.backgroundColor = "peachpuff";
-    footer.style.color = "black";
+    footer.style.backgroundColor = "Firebrick";
+    footer.style.color = "white";
     footer.style.columnCount = "7";
     footer.classList.add("footer");
     footer.classList.add("footerAlign");
+
  
  }
  footerDiv();
- 
