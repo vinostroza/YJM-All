36a03d90573f48513bc2b6e292b073d242e39e16
Wennona <50216937+WWWeLLs@users.noreply.github.com>
2019-06-02 18:32:21 -0500

    6/2/19 Committed Changes
    6/2/19 Committed Changes
    
    Co-Authored-By: vinostroza <vinostroza@users.noreply.github.com>
    Co-Authored-By: lwalker16 <lwalker16@users.noreply.github.com>
    Co-Authored-By: glor1 <glor1@users.noreply.github.com>

--- YJM Collaborate app/Scripts/YJMscripts.js
+++ YJM Collaborate app/Scripts/YJMscripts.js
@@ -2,10 +2,25 @@ function footerDiv() {
     let footer = document.getElementById("yjmFooter");
     footer.style.backgroundColor = "Firebrick";
     footer.style.color = "white";
-    footer.style.columnCount = "7";
+    footer.style.columnCount = "5";
     footer.classList.add("footer");
     footer.classList.add("footerAlign");
 
  
  }
  footerDiv();
+
+
+
+ //RESPONSIVE IDEA KEEP//
+
+//  $(document).ready(function(){
+//     $("a.mobile").click(function(){
+//       $(".sidebar").slideToggle('fast');
+//     });
+//     window.resize = function(event){
+//       if($(window).width() > 320){
+//         $(".sidebar").show();
+//       }
+//     };
+//   });
